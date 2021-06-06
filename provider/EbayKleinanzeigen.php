a04c9a3abad76be0014818559d5e74a1233c13e7
Tobias Dillig <tobias.dillig@frachtwerk.de>
2021-06-06 14:01:08 +0200

    fix: ebay-kleinanzeigen result parsing

--- provider/EbayKleinanzeigen.php
+++ provider/EbayKleinanzeigen.php
@@ -29,23 +29,26 @@ class EbayKleinanzeigen extends Provider
         $elements = $this->browser->findElements(WebDriverBy::cssSelector('#srchrslt-adtable > .ad-listitem.lazyload-item'));
         Log::info('found ' . count($elements) . ' offers!', ['title' => $this->browser->getTitle()]);
         $offers = [];
-        foreach ($elements as $key => $element) {
+        foreach ($elements as $element) {
             $foreignId = $element->findElement(WebDriverBy::cssSelector('.aditem'))->getAttribute('data-adid');
-            $title = trim($element->findElement(WebDriverBy::cssSelector('h2'))->getAttribute('textContent'));
-            $link = $element->findElement(WebDriverBy::cssSelector('h2 > a'))->getAttribute('href');
+            $title = $element->findElement(WebDriverBy::cssSelector('h2'));
+            $this->browser->executeScript('window.scrollTo(0,' . $title->getLocation()->getY() . ');');
+            $title = trim($title->getText());
+            $link = 'https://www.ebay-kleinanzeigen.de/' . $element->findElement(WebDriverBy::cssSelector('h2 > a'))->getAttribute('href');
             try {
-                $price = trim($element->findElement(WebDriverBy::cssSelector('.aditem-details > strong'))->getAttribute('textContent'));
+                $price = trim($element->findElement(WebDriverBy::cssSelector('.aditem-details > strong'))->getText());
             } catch (NoSuchElementException $exception) {
-                $price = trim($element->findElement(WebDriverBy::cssSelector('.aditem-main--middle--price'))->getAttribute('textContent'));
+                $price = trim($element->findElement(WebDriverBy::cssSelector('.aditem-main--middle--price'))->getText());
             }
-            $flatSize = trim($element->findElements(WebDriverBy::cssSelector('.simpletag.tag-small'))[0]->getAttribute('textContent'));
-            $rooms = trim(str_replace('Zimmer', '', $element->findElements(WebDriverBy::cssSelector('.simpletag.tag-small'))[1]->getAttribute('textContent')));
+            $flatSize = trim($element->findElements(WebDriverBy::cssSelector('.simpletag.tag-small'))[0]->getText());
+            $rooms = trim(str_replace('Zimmer', '', $element->findElements(WebDriverBy::cssSelector('.simpletag.tag-small'))[1]->getText()));
             try {
-                $address = str_replace($price, '', $element->findElement(WebDriverBy::cssSelector('.aditem-details'))->getAttribute('textContent'));
+                $address = str_replace($price, '', $element->findElement(WebDriverBy::cssSelector('.aditem-details'))->getText());
             } catch (NoSuchElementException $exception) {
-                $address = str_replace($price, '', $element->findElement(WebDriverBy::cssSelector('.aditem-main--top--left'))->getAttribute('textContent'));
+                $address = str_replace($price, '', $element->findElement(WebDriverBy::cssSelector('.aditem-main--top--left'))->getText());
             }
-            $offers[] = new OfferDTO($this->getProviderName(), $foreignId, $title, $link, $price, $flatSize, $rooms, $address);
+            $dto = new OfferDTO($this->getProviderName(), $foreignId, $title, $link, $price, $flatSize, $rooms, $address);
+            $offers[] = $dto;
         }
         return $offers;
     }
